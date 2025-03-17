1c49ab447ef22c62ab6ce290333c275a519e0d61
Lei <aguila.christleidaniel@gmail.com>
2025-03-17 18:39:55 +0800

    segregating the model from the web service to limit mb usage

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from ultralytics import YOLO
 from PIL import Image
+import gc
 import io
-import uvicorn
+import requests
 
 app = FastAPI()
 
+MODEL_URL = "https://huggingface.co/leiDanielAguila/nutrivision/resolve/main/yolov8_nutrivision.pt"
+
+def load_model():
+    model_path = "yolov8_nutrivision.pt"
+    response = requests.get(MODEL_URL, stream=True)
+
+    if response.status_code == 200:
+        with open(model_path, "wb") as f:
+            f.write(response.content)
+        print("Model downloaded from Hugging Face")
+    else:
+        raise Exception("Failed to download model from Hugging Face")
+
+    return YOLO(model_path).to("cpu").half()  # Load model in half precision to save RAM
+
+
 # Load the YOLO model
-model = YOLO("yolov8_nutrivision.pt")
+model = load_model()
+
+
+def preprocess_image(image):
+    return image.resize((640, 640))  # Resize image to 640x640
 
 
 @app.post("/detect")
@@ -15,6 +36,7 @@ async def detect_fruits(file: UploadFile = File(...)):
     try:
         image_bytes = await file.read()
         image = Image.open(io.BytesIO(image_bytes))
+        image = preprocess_image(image)
 
         # Perform detection
         results = model.predict(image, conf=0.6)
@@ -26,6 +48,10 @@ async def detect_fruits(file: UploadFile = File(...)):
         for cls_id in detected_objects:
             class_name = class_names[int(cls_id)]
             object_count[class_name] = object_count.get(class_name, 0) + 1
+
+        del image_bytes, image, results
+        gc.collect()
+
 
         if not object_count:
             return {"message": "No fruits detected"}
